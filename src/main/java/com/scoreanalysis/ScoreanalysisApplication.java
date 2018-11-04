aa4abf6c57844b7819e5db9f5b56ed8b1c3923f4
yang_ <yang_7296@163.com>
2018-11-04 17:34:36 +0800

    创建项目

---
+++ src/main/java/com/scoreanalysis/ScoreanalysisApplication.java
@@ -0,0 +1,12 @@
+package com.scoreanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreanalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreanalysisApplication.class, args);
+    }
+}
---
+++ src/test/java/com/scoreanalysis/ScoreanalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scoreanalysis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoreanalysisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
